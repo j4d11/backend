e6a83083f9a0e28aaf5f1a26ae512a742f282a47
jade Medeiros <2314290155@ACADEMICO.IESB.BR>
2024-03-15 10:53:42 -0300

    codigo aula05

---
+++ aulas/aula05/router.js
@@ -0,0 +1,41 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get("/produtos", function(req,res){
+    res.json([])//200 e json
+})
+
+router.get("/produtos/:produtoId", function(req,res){
+    if(req.params.produtoId == 1){
+    res.json({})//200 e json
+    }else{
+    res.status(404).json({msg: "Produto não encontrado"})
+}
+})
+
+router.post("/produtos", function(req, res){
+    if(req.body && req.body.nome && req.body.preco){
+    res.status(201).json({})
+     }else{
+    res.status(422).json({msg: "Nome e/ou preco do produto obrigatorios"})
+}
+})
+
+router.put("/produtos/:produtoId", function(req, res){
+    if (req.params.produtoId == 1){
+    res.json({})
+    }else{
+        res.status(404).json({msg: "Produto não encontrado"})
+    }
+})
+
+router.delete("/produtos/:produtoId", function(req, res){
+    if(req.params.produtoId == 1){
+    res.status(204).end()
+    }else{
+    res.status(404).json({msg: "Produto não encontrado"})
+    }
+})
+
+module.exports = router 
